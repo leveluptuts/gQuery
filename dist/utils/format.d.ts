051fdc96f530a0e07b6a38e1d250e8851673b68e
Scott Tolinski <scott@leveluptuts.com>
2022-01-17 10:32:33 -0700

    updates versions

--- dist/utils/format.d.ts
+++ dist/utils/format.d.ts
@@ -1,3 +1,2 @@
-import { DocumentNode } from "graphql";
-export declare const collectTypesFromResponse: (response: object) => string[];
+export declare const collectTypesFromResponse: (response: object) => any;
 export declare const formatDocument: <T extends DocumentNode>(node: T) => T;
