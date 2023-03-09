a26916fa256bc93f26307d141d7ff1ba6bad4e55
Scott Tolinski <scott@leveluptuts.com>
2023-03-09 10:49:25 -0700

    updates deps

--- packages/core/.svelte-kit/ambient.d.ts
+++ packages/core/.svelte-kit/ambient.d.ts
@@ -170,7 +170,7 @@ declare module '$env/static/private' {
  * ```
  */
 declare module '$env/static/public' {
-
+	
 }
 
 /**
--- packages/core/.svelte-kit/generated/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../../../../node_modules/.pnpm/@sveltejs+kit@1.0.1_svelte@3.55.0+vite@4.0.3/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
